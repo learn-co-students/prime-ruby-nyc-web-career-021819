5d12d9b7102ff8e81c0d7ce68e7ea078311a44e1
Alonso Ortega <alonsojortega@gmail.com>
2019-01-24 20:15:29 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(num)
+  if (num < 2)
+    return false
+  end
+
+  range  = (2..num - 1).to_a
+
+  for x in range
+    if num % x == 0
+      return false
+    end
+  end
+  return true
+end
