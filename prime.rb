19b170d93f0d556be55e3cfecea6b7ca52ee52f8
Adam Gordon <adamjgordon24@gmail.com>
2019-01-27 21:27:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(integer)
+  if integer >= 2 
+   possible_factors = (2..integer-1).to_a
+   possible_factors.all? do |n|
+     integer%n != 0
+   end 
+  else 
+    false
+  end
+end 
