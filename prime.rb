3e01f0cb388edaf5b7128d307013a844087fcc15
Nicholas Dover <dovern42@gmail.com>
2019-02-05 04:38:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    false
+  else
+    n = Math.sqrt(number).floor
+    isit = 0
+    array = (2..n)
+    array.each do |num|
+      if number % num == 0
+        isit+=1
+      else
+      end
+    end
+    (isit < 1)
+  end
+end
