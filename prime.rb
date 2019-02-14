e7e364793bb16f784b49027d8c86098d8711bd0e
Fillan Lee <fillanlee@aol.com>
2019-02-14 07:57:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(num)
+  # num.to_i
+  factors =[]
+  prime = false
+  array = (1..num)
+ 
+array.each do |value|
+  if num % value == 0 
+    factors << value
+  else
+    prime = false
+  end
+end
+
+  if num <= 0 || num == 1
+    prime = false
+  elsif factors.length > 2
+   prime = false
+ else
+   prime = true
+ 
+end
+  prime
+end
+ 
